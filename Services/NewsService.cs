a45c237056efec9321f348ac9aacf25e0a6e16ac
Adam <Adam@DESKTOP-931URV2>
2022-12-28 16:00:44 +0100

    Add project files.

---
+++ Services/NewsService.cs
@@ -0,0 +1,75 @@
+﻿//#define UseNewsApiSample  // Remove or undefine to use your own code to read live data
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+using Assignment_A2_04.Models;
+using Assignment_A2_04.ModelsSampleData;
+using System.Net;
+
+namespace Assignment_A2_04.Services
+{
+    public class NewsService
+    {
+
+        ConcurrentDictionary<string, News> cachedNews = new ConcurrentDictionary<string, News>();
+
+
+        HttpClient httpClient = new HttpClient();
+ 
+        // Your API Key
+        readonly string apiKey = "1bd9f35626c54515b157bb3fcbc60f3a";
+
+
+        public event EventHandler<string> NewsReportAvailable;
+        protected virtual void OnNewsReportAvailable(string message)
+        {
+            NewsReportAvailable?.Invoke(this, message);
+        }
+
+        public NewsService()
+        {
+            httpClient = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate });
+            httpClient.DefaultRequestHeaders.Add("user-agent", "News-API-csharp/0.1");
+            httpClient.DefaultRequestHeaders.Add("x-api-key", apiKey);
+        }
+
+        public async Task<News> GetNewsAsync(NewsCategory category)
+        {
+
+            //https://newsapi.org/docs/endpoints/top-headlines
+            var uri = $"https://newsapi.org/v2/top-headlines?country=se&category={category}";
+
+            // make the http request
+            var httpRequest = new HttpRequestMessage(HttpMethod.Get, uri);
+            var response = await httpClient.SendAsync(httpRequest);
+            response.EnsureSuccessStatusCode();
+
+            //Convert Json to Object
+            NewsApiData nd = await response.Content.ReadFromJsonAsync<NewsApiData>();
+
+
+            News news = new News() { Articles = nd.Articles.Select(n => new NewsItem
+            {
+                    DateTime = n.PublishedAt,
+                    Title = n.Title,
+                    Description = n.Description,
+                    Url = n.Url,
+
+                }).ToList()
+            };
+
+            NewsReportAvailable?.Invoke(news, $"News in category available: {category}");
+
+
+            return news;
+
+        }
+    }
+
+}
---
+++ Services/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Assignment_A2_04.Models;
+using Assignment_A2_04.ModelsSampleData;
+using System.Net;
+
+namespace Assignment_A2_04.Services
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            NewsService service = new NewsService();
+            service.NewsReportAvailable += NewsReportDataAvailable;
+
+            Task<News>[] tasks = { null };
+
+            tasks[0] = service.GetNewsAsync(NewsCategory.sports);
+
+            Task.WaitAll(tasks[0]);
+
+            News news = await new NewsService().GetNewsAsync(NewsCategory.sports);
+
+
+            Console.WriteLine("Headlines: ");
+            var groupedNewsList = news.Articles.GroupBy(n => (n.DateTime.DayOfWeek, n.DateTime.ToShortDateString()), n => n).Distinct().ToList();
+
+            foreach (var News in groupedNewsList)
+            {
+                News.ToList().ForEach(n => Console.WriteLine($"{n.DateTime}: - {n.Title} - {n.UrlToImage}"));
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------------------------------------");
+
+
+            void NewsReportDataAvailable(object sender, string message)
+            {
+                Console.WriteLine($"Event message from news service: {message}");
+            }
+        }
+    }
+}
